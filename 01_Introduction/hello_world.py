f3b97d083374d08b192ffe1fe05337f00e398b63
Salman Mahmud <salman_mahmud@hotmail.com>
2023-03-13 16:21:53 -0400

    introduction

--- 01_Introduction/hello_world.py
+++ 01_Introduction/hello_world.py
@@ -1,6 +1,3 @@
 # This is how you create a comment
 
-# Print 'Hello, World!' to the terminal.
-print("Hello, World!")
-
-# `print` is a built-in `function` provided by Python
+print('Hello Salman')
